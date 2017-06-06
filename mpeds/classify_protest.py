2ebc599327c6c7cab8f4fb346c71894d374a48b4
Alex Hanna <alex.hanna@gmail.com>
2017-06-06 13:45:33 -0500

    fixes to Solr request

--- mpeds/classify_protest.py
+++ mpeds/classify_protest.py
@@ -42,13 +42,13 @@ class MPEDS:
         return query
 
 
-    def makeSolrRequest(self, q, fq, protest = False):
+    def makeSolrRequest(self, q, fq = None, protest = False):
         """ makes Solr requests to get article texts """
 
         data = {
             'q':     q,
-            'start': start,
-            'rows':  rows,
+            'start': 0,
+            'rows':  10,
             'wt':    'json'
         }
 
@@ -57,7 +57,7 @@ class MPEDS:
             data['fq'] = self.search_str
 
         data = urllib.urlencode(data)
-        req  = urllib2.Request(solr_url, data)
+        req  = urllib2.Request(self.solr_url, data)
         res  = urllib2.urlopen(req)
         res  = json.loads(res.read())
 
@@ -65,22 +65,33 @@ class MPEDS:
 
         print("%d documents found." % numFound)
 
+        interval = 100
+
         ## add 100 to get everything for sure
-        numFound += 100
+        numFound += interval
+
+        prev = 0
+        data = {
+            'q': q,
+            'rows': interval,
+            'start': prev,
+            'wt': 'json'
+        }
+
+        if fq and not protest:
+            data['fq'] = fq
 
         articles = []
-        interval = 100
-        prev = 0
         for i in range(0, numFound, interval):
             data = {
                 'q': q,
-                'fq': fq,
                 'rows': interval,
                 'start': prev,
                 'wt': 'json'
             }
+
             data = urllib.urlencode(data)
-            req  = urllib2.Request(url, data_str)
+            req  = urllib2.Request(self.solr_url, data)
             res  = urllib2.urlopen(req)
             res  = json.loads(res.read())
 
@@ -91,7 +102,7 @@ class MPEDS:
 
             prev = i
 
-        return docs
+        return articles
 
 
     def getLede(self, text):
