43b63e353ed17d507a43feb3626a66ef567c5dc1
Erle Holgersen <erle.holgersen@gmail.com>
2017-07-17 18:48:07 -0500

    Fixing .gitattributes to allow for new directory structure, and updating setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='MPEDS',
-    packages=['mpeds'],
-    data_files=[('tokenizers', ['tokenizers/stanford-ner-3.6.0-javadoc.jar', 'tokenizers/stanford-ner-3.6.0-sources.jar', 'tokenizers/stanford-ner-3.6.0.jar']),
-        ('classifiers', ['classifiers/form-vect_2017-05-23.pkl', 'classifiers/form_2017-05-23.pkl', 'classifiers/haystack-vect_all-source_2017-05-24.pkl', 'classifiers/haystack_all-source_2017-05-24.pkl', 'classifiers/issue-vect_2017-05-23.pkl', 'classifiers/issue_2017-05-23.pkl', 'classifiers/ner-orgs_2016-03-28_all.ser.gz', 'classifiers/target-vect_2017-06-27.pkl', 'classifiers/target_2017-06-27.pkl'])
-        ]
-    )
+    name = 'MPEDS',
+    packages = ['mpeds'],
+    version = '0.1',
+    author = 'Alex Hanna',
+    author_email = 'alex.hanna@utoronto.ca',
+    package_data = {'mpeds': ['tokenizers/*.jar', 'classifiers/*.pkl', 'classifiers/*.ser.gz', 'input/*.csv']},
+    include_package_data = True
+)
