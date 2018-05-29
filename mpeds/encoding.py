8c51d1c4d15edd0d8e7a1bdcd076f6a1d9a0df99
adafnos <35844812+adafnos@users.noreply.github.com>
2018-05-29 15:41:03 +0100

    Create encoding.py

---
+++ mpeds/encoding.py
@@ -0,0 +1,10 @@
+# Fixes UnicodeDecodeError: 'ascii' codec can't decode byte 0xc2 in position 269: ordinal not in range(128)
+# Adapted from https://stackoverflow.com/questions/21129020/how-to-fix-unicodedecodeerror-ascii-codec-cant-decode-byte/35444608
+
+import os
+
+os.chdir('/usr/lib64/python2.7/site-packages')
+file = open('sitecustomize.py', 'w') 
+file.write('import sys\n')
+file.write("sys.setdefaultencoding('utf8')")
+file.close(
