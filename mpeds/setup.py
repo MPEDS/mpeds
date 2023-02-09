50017e32f1574682337521e0c3b743d5ed6b9feb
alexhanna <alex.hanna@gmail.com>
2023-02-08 21:38:31 -0600

    updating version and email information

--- mpeds/setup.py
+++ mpeds/setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 setup(
     name = 'MPEDS',
     packages = ['mpeds'],
-    version = '0.1',
+    version = '0.1.1',
     author = 'Alex Hanna',
-    author_email = 'alex.hanna@utoronto.ca',
+    author_email = 'alex.hanna@gmail.com',
     package_data = {'mpeds': ['tokenizers/*.jar', 'classifiers/*.pkl', 'classifiers/*.ser.gz', 'input/*.csv']},
     include_package_data = True
 )
